c972a22434485dcfbd911fc6b6efae50ed467083
Ivonian <lumuivan@gmail.com>
2018-02-24 17:40:20 +0300

    4th Commit

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,18 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes(); //activating attibute routing...where routes are defined above actions
+
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new {controller = "Movies", action = "ByReleaseDate"}, //defaults defined using an anonymous object
+            //    //new {year=@"\d{4}", month=@"\d{2}"}, //applying contraints using an anonymous object with regular expressions
+            //    new {year=@"2017|2018", month=@"\d{2}"} 
+            //);
+
+
+            //The default route below can only take one parameter to set up for more parameters, you need to create custom routes before the default route 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Razor;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -15,9 +17,24 @@ namespace Vidly.Controllers
             
             var movie  = new Movies() { Name = "Black Panther"};
 
+            var customers = new List<Customer>
+            {
+                new Customer { Name =  "Ivan"},
+                new Customer { Name =  "Irina"}
+            };
+
+            //we created the RandomViewModel so that we can pass two or more domain models to one view
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
             //ActioResult is the prefered run type, so that we can return any of its subtypes; ViewResult, PartialViewResult, ContentResult, RedirectResult, FileResult, HttpNotFoundResult
 
-            return View(movie);
+            return View(viewModel);
+
+            //return View(movie);
 
             //return Content("Hi Ivan");
 
@@ -26,8 +43,13 @@ namespace Vidly.Controllers
             //return RedirectToAction("Index", "Home");
 
         }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
 
         //movies
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")] //Attribute route maintained with contraints on parameters
         public ActionResult Index(int? pageIndex, string sortBy)
         {
             if (!pageIndex.HasValue)
